c226798063942f95cb8b013924ddd4930e449a8b
Cynthia Iradukunda <cynthia@Cynthias-MacBook-Pro.local>
2021-07-18 23:13:15 +0200

    server confiq

--- server.js
+++ server.js
@@ -24,6 +24,30 @@ app.get('/api/albums/:id/photos', async(req, res) =>{
        res.send(result);
 });
 
+const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://shrouded-journey-38552.heroku...']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 const port = process.env.port || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
