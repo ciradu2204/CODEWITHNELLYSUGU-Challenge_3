079ce359b959fb61e7d479f2d9c114c3bf8cddb4
Cynthia Iradukunda <cynthia@Cynthias-MacBook-Pro.local>
2021-07-18 23:05:07 +0200

    added the server

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// "Create React App" tooling expects to find the WWW root here,
+// so we simply use this file to import the ./www directory.
+//
+// For the backend, we set the container's entrypoint to src/api/index.js
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+module.exports = require('./www')
