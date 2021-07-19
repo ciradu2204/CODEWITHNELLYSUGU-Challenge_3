5593aee6d9058baf7cb00d392775686ba4c5533e
Cynthia Iradukunda <cynthia@Cynthias-MacBook-Pro.local>
2021-07-19 02:02:19 +0200

    added client code

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+
+const Header =() =>{
+
+    return (
+        <div className="Header">
+        <h2>Challenge 3</h2>
+        <p>A challenge built using typicode api, React, Css, and Express</p>
+        </div>
+    )
+}
+
+export default Header;
